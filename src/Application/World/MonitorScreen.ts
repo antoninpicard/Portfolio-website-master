ea70a97b78ce711c0ea76de4b843e4003bcefff9
antoninpicard <antoninpicard0@gmail.com>
2026-03-03 15:40:22 +0200

    update for firefox

--- src/Application/World/MonitorScreen.ts
+++ src/Application/World/MonitorScreen.ts
@@ -209,7 +209,7 @@ export default class MonitorScreen extends EventEmitter {
         iframe.className = 'jitter';
         iframe.id = 'computer-screen';
         iframe.frameBorder = '0';
-        iframe.title = 'HeffernanOS';
+        // iframe.title = 'AntoninOS';
 
         // Add iframe to container
         container.appendChild(iframe);
